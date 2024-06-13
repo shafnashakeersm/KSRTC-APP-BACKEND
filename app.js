d9a9f56bd9c7cb7813b1fd2597c4493c4d01100b
shafnashakeer <shafnasm20@gmail.com>
2024-06-14 01:01:04 +0530

    branch complete

--- app.js
+++ app.js
@@ -9,3 +9,15 @@ app.use(express.json())
 
 mongoose.connect("mongodb+srv://shafnashakeersm:Shafna123@cluster0.2srguee.mongodb.net/busdb?retryWrites=true&w=majority&appName=Cluster0")
 
+app.post("/add",async(req,res)=>{
+    let input=req.body
+    console.log(input)
+    let bus=new busmodel(input)
+    bus.save()
+    res.json({status:"success"})
+})
+
+
+app.listen(8004,()=>{
+    console.log("server started")
+})
